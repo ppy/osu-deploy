6a2cf44ea1fe1c9b2b10a7db63ebc7fb1de1c110
Dan Balasescu <smoogipoo@smgi.me>
2024-08-31 00:40:56 +0900

    Use custom app bundle
    In particular, the `--icon` argument requires a specific icon type. We
    already have our own prebuilt bundle with the icon _and_ other things
    like the plist.

--- Builders/MacOSBuilder.cs
+++ Builders/MacOSBuilder.cs
@@ -2,14 +2,19 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.IO;
 using osu.Desktop.Deploy.Uploaders;
 
 namespace osu.Desktop.Deploy.Builders
 {
     public class MacOSBuilder : Builder
     {
+        private const string app_dir = "osu!.app";
         private const string app_name = "osu!";
         private const string os_name = "osx";
+
+        private readonly string stagingTarget;
+        private readonly string publishTarget;
 
         public MacOSBuilder(string version, string? arch)
             : base(version)
@@ -24,6 +29,9 @@ namespace osu.Desktop.Deploy.Builders
                 Logger.Error($"Invalid Architecture: {arch}");
 
             RuntimeIdentifier = $"{os_name}-{arch}";
+
+            stagingTarget = Path.Combine(Program.StagingPath, app_dir);
+            publishTarget = Path.Combine(stagingTarget, "Contents", "MacOS");
         }
 
         protected override string TargetFramework => "net8.0";
@@ -31,7 +39,7 @@ namespace osu.Desktop.Deploy.Builders
 
         public override Uploader CreateUploader()
         {
-            string extraArgs = $" --icon=\"{IconPath}\"";
+            string extraArgs = "";
 
             if (!string.IsNullOrEmpty(Program.AppleCodeSignCertName))
                 extraArgs += $" --signAppIdentity=\"{Program.AppleCodeSignCertName}\"";
@@ -40,9 +48,20 @@ namespace osu.Desktop.Deploy.Builders
             if (!string.IsNullOrEmpty(Program.AppleNotaryProfileName))
                 extraArgs += $" --notaryProfile=\"{Program.AppleNotaryProfileName}\"";
 
-            return new VelopackUploader(app_name, os_name, RuntimeIdentifier, RuntimeIdentifier, extraArgs);
+            return new VelopackUploader(app_name, os_name, RuntimeIdentifier, RuntimeIdentifier, extraArgs: extraArgs, stagingPath: stagingTarget);
         }
 
-        public override void Build() => RunDotnetPublish();
+        public override void Build()
+        {
+            if (Directory.Exists(stagingTarget))
+                Directory.Delete(stagingTarget, true);
+
+            Program.RunCommand("cp", $"-r \"{Path.Combine(Program.TemplatesPath, app_dir)}\" \"{stagingTarget}\"");
+
+            RunDotnetPublish(outputDir: publishTarget);
+
+            // without touching the app bundle itself, changes to file associations / icons / etc. will be cached at a macOS level and not updated.
+            Program.RunCommand("touch", $"\"{stagingTarget}\" {Program.StagingPath}", false);
+        }
     }
 }
