287084942f236cd62983fd8adc6377149b08dfba
Jami Kettunen <jami.kettunen@protonmail.com>
2025-09-30 16:24:53 +0300

    Include arch in published Windows & Linux artifact filenames
    Instead of install.exe and osu.AppImage now we'll get install-x64.exe
    and osu-x64.AppImage allowing for future clean addition of arm64 files
    not much unlike is already the case with macOS.

--- Uploaders/WindowsVelopackUploader.cs
+++ Uploaders/WindowsVelopackUploader.cs
@@ -1,6 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Linq;
+
 namespace osu.Desktop.Deploy.Uploaders
 {
     public class WindowsVelopackUploader : VelopackUploader
@@ -18,7 +21,13 @@ namespace osu.Desktop.Deploy.Uploaders
         public override void PublishBuild(string version)
         {
             base.PublishBuild(version);
-            RenameAsset($"{Program.PackageName}-{channel}-Setup.exe", "install.exe");
+
+            if (channel != "win-x64")
+                throw new Exception($"Unrecognised channel: {channel}");
+
+            string suffix = channel.Split('-').Last();
+
+            RenameAsset($"{Program.PackageName}-{channel}-Setup.exe", $"install-{suffix}.exe");
         }
     }
 }
