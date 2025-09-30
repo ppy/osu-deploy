b19b8e83a82539107005bf0bf99db79fe2216107
Jami Kettunen <jami.kettunen@protonmail.com>
2025-09-30 16:24:53 +0300

    Include arch in published non-x64 Windows & Linux artifact filenames
    Instead of install.exe and osu.AppImage now we'll get install-arm64.exe
    and osu-arm64.AppImage cleanly separating the existing x64 variants from
    the newly supported arm64 ones.

--- Uploaders/WindowsVelopackUploader.cs
+++ Uploaders/WindowsVelopackUploader.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 namespace osu.Desktop.Deploy.Uploaders
 {
     public class WindowsVelopackUploader : VelopackUploader
@@ -18,7 +20,14 @@ namespace osu.Desktop.Deploy.Uploaders
         public override void PublishBuild(string version)
         {
             base.PublishBuild(version);
-            RenameAsset($"{Program.PackageName}-{channel}-Setup.exe", "install.exe");
+
+            if (channel != "win"/*-x64*/ && channel != "win-arm64")
+                throw new Exception($"Unrecognised channel: {channel}");
+
+            // Include architecture in asset name while x64 keeps using the old convention without it
+            string arch_suffix = channel.Substring(3);
+
+            RenameAsset($"{Program.PackageName}-{channel}-Setup.exe", $"install{arch_suffix}.exe");
         }
     }
 }
